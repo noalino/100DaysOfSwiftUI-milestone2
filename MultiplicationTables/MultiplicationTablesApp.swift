34a32f76be7d994fbfc3ea9da0b25221bf89b897
Noalino <bgelineau.dev@gmail.com>
2023-11-15 10:58:51 +0100

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Noalino on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
